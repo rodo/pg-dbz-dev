5d6204519d2f039a538cfa5b370a4c8d4ceb2f51
Rodolphe Quiedeville <r.quiedeville@smartway.ai>
2025-03-04 11:18:25 +0100

    chore: add tables and pg_cron extension

--- Dockerfile
+++ Dockerfile
@@ -3,7 +3,7 @@ FROM postgres:17
 LABEL org.opencontainers.image.authors="rodolphe@quiedeville.org"
 
 # add pgtap extension, always useful to do some test
-RUN apt-get update && apt-get install -y --no-install-recommends postgresql-17-pgtap && apt-get clean
+RUN apt-get update && apt-get install -y --no-install-recommends postgresql-17-pgtap postgresql-17-cron && apt-get clean
 
 # Install the extension in the temaplte database to make it available
 #  in all newly created databases
---
+++ init-10-user-db.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+set -e
+
+psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
+        CREATE USER auto REPLICATION;
+        CREATE DATABASE auto OWNER auto;
+        GRANT ALL PRIVILEGES ON DATABASE auto TO auto;
+EOSQL
+
+psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
+        CREATE USER manual REPLICATION;
+        CREATE DATABASE manual OWNER manual;
+        GRANT ALL PRIVILEGES ON DATABASE manual TO manual;
+EOSQL
