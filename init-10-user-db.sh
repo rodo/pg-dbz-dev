76fe93d32f302f130553e8154bdfba4e5a19af9c
Rodolphe Quiedeville <r.quiedeville@smartway.ai>
2025-03-05 09:44:10 +0100

    fix: set the password explicitely

--- init-10-user-db.sh
+++ init-10-user-db.sh
@@ -2,13 +2,13 @@
 set -e
 
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
-        CREATE USER auto REPLICATION;
+        CREATE USER auto REPLICATION PASSWORD 'auto';
         CREATE DATABASE auto OWNER auto;
         GRANT ALL PRIVILEGES ON DATABASE auto TO auto;
 EOSQL
 
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
-        CREATE USER manual REPLICATION;
+        CREATE USER manual REPLICATION PASWWORD 'manual';
         CREATE DATABASE manual OWNER manual;
         GRANT ALL PRIVILEGES ON DATABASE manual TO manual;
 EOSQL
