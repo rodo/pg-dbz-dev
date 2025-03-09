cded2b7057e9457e83c27003f4d629d687e2fc58
Rodolphe Quiedeville <r.quiedeville@smartway.ai>
2025-03-09 17:09:27 +0100

    fix: always use underscore instead of dash

--- init-10-user-db.sh
+++ init-10-user-db.sh
@@ -4,11 +4,11 @@ set -e
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
         CREATE USER auto REPLICATION PASSWORD 'auto';
         CREATE DATABASE auto OWNER auto;
-        CREATE DATABASE sink-auto OWNER auto;
+        CREATE DATABASE sink_auto OWNER auto;
 EOSQL
 
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
         CREATE USER manual REPLICATION PASSWORD 'manual';
         CREATE DATABASE manual OWNER manual;
-        CREATE DATABASE sink-manual OWNER manual;
+        CREATE DATABASE sink_manual OWNER manual;
 EOSQL
