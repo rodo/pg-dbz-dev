87980a5d10d3102dc7bd6d48dcff6f3e6d99a06b
Rodolphe Quiedeville <r.quiedeville@smartway.ai>
2025-03-10 10:10:31 +0100

    feat: activate connection logs

--- init-00-system.sh
+++ init-00-system.sh
@@ -3,5 +3,7 @@ set -e
 
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname postgres <<-EOSQL
         ALTER SYSTEM SET wal_level=logical;
+        ALTER SYSTEM SET log_connections=on;
+        ALTER SYSTEM SET log_disconnections=on;
         CREATE EXTENSION pg_cron;
 EOSQL
